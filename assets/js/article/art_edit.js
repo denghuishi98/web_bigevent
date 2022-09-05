b14fd062124dde297f80427a716b8bb18147b720
denghuishi98 <2522262236@qq.com>
2022-09-05 17:35:41 +0800

    '完成了文章管理功能的相关开发'

---
+++ assets/js/article/art_edit.js
@@ -0,0 +1,18 @@
+$(function(){
+    // var layer = layui.layer
+
+    // $.ajax({
+    //     method: 'GET',
+    //     url: '/my/article/',
+    //     data: $(this).serialize(),
+    //     success: function(res){
+    //         if(res.status !== 0){
+    //             return layer.msg('获取文章列表数据失败！')
+    //         }
+    //         console.log(res);
+    //         // layer.msg('获取文章列表数据成功！')
+    //         //获取数据成功以后 根据ID拿到对应的文章标题数据
+    //         $()  
+    //     }
+    // })
+})
